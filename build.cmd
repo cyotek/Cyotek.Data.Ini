c6c2c29ba453340590f1ab7b99f727bc39092215
Richard Moss <richard.moss@cyotek.com>
2022-06-27 21:16:38 +0100

    NEW: Added build.cmd and package

---
+++ build.cmd
@@ -0,0 +1,68 @@
+@ECHO OFF
+
+SETLOCAL
+
+SET CONFIG=Release
+SET BASENAME=Cyotek.Data.Ini
+SET RELDIR=src\bin\%CONFIG%\
+SET PRJFILE=src\%BASENAME%.csproj
+SET DLLNAME=%BASENAME%.dll
+
+SET DISTDIR=dist\
+
+SET DEPDIR=%DISTDIR%demo\
+
+IF EXIST %RELDIR%*.nupkg  DEL /F %RELDIR%*.nupkg
+IF EXIST %RELDIR%*.snupkg DEL /F %RELDIR%*.snupkg
+IF EXIST %RELDIR%*.zip    DEL /F %RELDIR%*.zip
+IF EXIST %DEPDIR%         RMDIR /Q /S %DEPDIR%
+IF EXIST %DISTDIR%        RMDIR /Q /S %DISTDIR%
+
+MKDIR %DISTDIR%
+
+CALL runtests.cmd
+IF %ERRORLEVEL% NEQ 0 GOTO :failed
+
+CALL :buildpackage
+IF %ERRORLEVEL% NEQ 0 GOTO :failed
+
+ENDLOCAL
+
+GOTO :eof
+
+:buildfailed
+:failed
+ECHO ERROR: Build failed.
+EXIT /b 1
+
+
+:buildpackage
+dotnet build %PRJFILE% --configuration %CONFIG%
+IF %ERRORLEVEL% NEQ 0 EXIT /b %ERRORLEVEL%
+
+PUSHD %RELDIR%
+
+CALL cyotek-sign-file.cmd net35\%DLLNAME%
+CALL cyotek-sign-file.cmd net40\%DLLNAME%
+CALL cyotek-sign-file.cmd net45\%DLLNAME%
+CALL cyotek-sign-file.cmd net452\%DLLNAME%
+CALL cyotek-sign-file.cmd net462\%DLLNAME%
+CALL cyotek-sign-file.cmd net472\%DLLNAME%
+CALL cyotek-sign-file.cmd net48\%DLLNAME%
+CALL cyotek-sign-file.cmd net6.0\%DLLNAME%
+CALL cyotek-sign-file.cmd netcoreapp3.1\%DLLNAME%
+CALL cyotek-sign-file.cmd netstandard2.1\%DLLNAME%
+
+7z.exe a %BASENAME%.2.x.x.zip -r
+
+POPD
+
+MOVE %RELDIR%*.zip %DISTDIR%
+
+dotnet pack %PRJFILE% --configuration Release --no-build
+IF %ERRORLEVEL% NEQ 0 EXIT /b %ERRORLEVEL%
+
+CALL cyotek-sign-package.cmd %RELDIR%*.nupkg
+CALL cyotek-sign-package.cmd %RELDIR%*.snupkg
+
+EXIT /b %ERRORLEVEL%
---
+++ src/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
+[assembly: Guid("5add1ebc-0931-4f96-b9fb-b50503ef5758")]
+[assembly: AssemblyVersion("1.0.0.0")]
+
+[assembly: InternalsVisibleTo("Cyotek.Data.Ini.Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100a1ddbe4bc53fb510718e8886f11792cb6cd111812812b8f2dcb572b4e3f66acbee42f4e00cc77916897c2f1360b0f772158b34b45f4206b1b3ecabe7d432395e228552661998cb5cc1d2ae1adabed9c09e31808454fe3d1c94e521e6478d43d87e1a99644c164e1b596b24ae7da666abe41df21b9a8ad6dbdaab6827037570a8")]
